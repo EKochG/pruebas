4d8267a4c67f4b291cf87065277ed9c3cbb4b47a
elard <elard.koch@outlook.es>
2018-05-01 11:27:42 -0300

    avance

--- algo.py
+++ algo.py
@@ -13,19 +13,21 @@ api = tweepy.API(auth)
 #api.update_status("holi")
 #a!="20:34"
 Dias_semana=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
-cont=5
+cont=6
 elem=0
 felicidades=["excelente","maginifico","genial","bacan","maravilloso"]
 a=str(time.strftime("%H:%M")) 
 while(True):
-	if(a=="08:00"):
+	if(a=="08:30"):
 		elem=random.randrange(5)
-		api.update_status("Hola a todos, espero que tengan un "+felicidades[elem]+" dia "+Dias_semana[cont])
+		api.update_status("Hola a todos, espero que tengan un "+str(felicidades[elem])+" dia "+str(Dias_semana[cont]))
+		time.sleep(60)	
 	if(a=="13:00"):
 		api.update_status("Hora de almuerzo, saludos!!")
+		time.sleep(60)
 	if(a=="21:00"):
-		api.update_status("Hola, espero que todos hayan tenido un excelente dia"+Dias_semana[cont])
+		api.update_status("Hola, espero que todos hayan tenido un excelente dia "+Dias_semana[cont])
 		cont=(cont+1)%7
+		time.sleep(60)
 	time.sleep(1)
 	a=str(time.strftime("%H:%M")) 
-	print(a)
